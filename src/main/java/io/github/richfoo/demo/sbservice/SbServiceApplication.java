c5c72b8a3068e5e5aaf965ec914a2cf6739f399c
Rich W <random.rich@gmail.com>
2019-02-28 14:20:21 -0700

    Empty sb app

---
+++ src/main/java/io/github/richfoo/demo/sbservice/SbServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.richfoo.demo.sbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbServiceApplication.class, args);
+	}
+
+}
